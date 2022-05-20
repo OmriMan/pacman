2ce2cd887498d2f3fb71c1253f3254272bec6677
Omri Man <omriman11@gmail.com>
2022-05-20 12:42:42 +0300

    Update form.js

--- form.js
+++ form.js
@@ -248,48 +248,15 @@ function setControls(u , d ,r ,l){
 }
 
 
-// window.addEventListener('load', setup);
-
-// const get = document.getElementById.bind(document);
-// const query = document.querySelector.bind(document);
-// let modalRoot = get('modal-root');
-// let button = get('modal-opener');
-// let modal = query('.modal');
-
-
-// function setup() {
-  
-//   let modalRoot = get('modal-root');
-//   let button = get('modal-opener');
-//   let modal = query('.modal');
-  
-//   modalRoot.addEventListener('click', rootClick);
-//   button.addEventListener('click', openModal);
-//   modal.addEventListener('click', modalClick);
-  
-//   function rootClick() {
-//     modalRoot.classList.remove('visible');
-//   }
-  
-//   function openModal() {
-//     modalRoot.classList.add('visible');
-//   }
-  
-//   function modalClick(e) {
-//     e.preventDefault();
-//     e.stopPropagation();
-//     e.stopImmediatePropagation();
-//     return false;
-//   }
-  
-// }
 
 
 function openAbout() {
-    $('#About').show();
-
-
-	document.getElementById("dialog").showModal();
+    if (game_on){
+        PauseAbout();
+    } else {
+        $('#About').show();
+        document.getElementById("dialog").showModal();
+    }
 }
 
 function closeAbout() {
@@ -345,3 +312,11 @@ function showgame(){
     // return false;
 
 }
+window.addEventListener('mouseup',function(event){
+    var dialog = document.getElementById('About');
+    if(event.target != About && event.target.parentNode == About){
+        //$('#About').hide();
+
+        document.getElementById("dialog").close();
+    }
+});  
